f886e5f4945aac25da06ac33f836acbd3051c5c6
Ajish George <ajishg@gmail.com>
2014-11-10 22:44:12 -0500

    refactored models to own dir

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import falcon
-import lib.things as things
-import lib.users as users
+import lib.models.things as things
+import lib.models.users as users
 api = application = falcon.API()
 
 things = things.ThingsResource()
